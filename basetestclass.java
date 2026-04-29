ac0f89ee7e6f7ac06562e910de732452183be3a9
Akshat Pandey <137032734+30-06AKSHAT-PANDEY@users.noreply.github.com>
2026-04-29 16:32:51 +0530

    Create basetestclass.java

---
+++ basetestclass.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseTest {
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        driver = DriverFactory.initDriver();
+        driver.get("https://example.com/login");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        DriverFactory.quitDriver();
+    }
+}
